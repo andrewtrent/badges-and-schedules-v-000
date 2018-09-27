20b355ed270c7bcf43b460d92ea69058ba37bfaf
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-27 22:34:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_arr = []
+  array.each {|x| new_arr.push("Hello, my name is #{x}.")}
+  new_arr
+end
+
+def assign_rooms(speaker_list)
+  speaker_list.each {|x| puts("Hello, #{x}! You'll be assigned to room #{x.index}!")}
+end
